d5e251aadc00d78b0fe9125cce4ea7baafe80ac1
egogithub <egoloubev@gmail.com>
2021-05-19 16:58:31 +0200

    Work in progress

--- app/src/main/java/net/ego/myhome/pojo/TempDevice.java
+++ app/src/main/java/net/ego/myhome/pojo/TempDevice.java
@@ -1,4 +1,12 @@
 package net.ego.myhome.pojo;
 
 public class TempDevice extends Device {
+    public static final String idx = "Index";
+    public static final String Type = "Type";
+    public static final String Name = "Name";
+    public static final String Data = "Data";
+    public static final String DewPoint = "Dewpoint";
+    public static final String HumidityStatus = "Humistatus";
+    public static final String LastUpdate = "Lastupdate";
+    public static final String ID = "SensorId";
 }
--- app/src/main/java/net/ego/myhome/providers/MyHomeContentProvider.java
+++ app/src/main/java/net/ego/myhome/providers/MyHomeContentProvider.java
@@ -1,9 +1,40 @@
 package net.ego.myhome.providers;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
 public class MyHomeContentProvider {
 
     public static final String DATABASE_NAME = "myhome.db";
     public static final String TEMP_TABLE_NAME = "temperature";
     public static final String SWITCH_TABLE_NAME = "switch";
+
+    static int DATABASE_VERSION = 1;
+
+    private static class MyHomeDbHelper extends SQLiteOpenHelper {
+
+        private MyHomeDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
+            super(context, name, factory, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            createTempTable(db);
+            createSwitchTable(db);
+        }
+
+        private void createTempTable(SQLiteDatabase db) {
+            String qs = "CREATE TABLE " + TEMP_TABLE_NAME + "(" +
+                    BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    T
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
 
 }
